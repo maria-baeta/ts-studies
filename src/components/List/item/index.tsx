289ea1844679e84202540bab99576589b9d0202a
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-08-03 09:47:31 -0300

    refactor: :recycle: Refatorando o código
    Implementando melhorias na lista (usnado Keys) e criação do cronometro

--- src/components/List/item/index.tsx
+++ src/components/List/item/index.tsx
@@ -1,6 +1,5 @@
-import { memo } from "react";
 import style from "../List.module.scss";
-const Item = ({ task, time }: { task: string; time: string }) => {
+export const Item = ({ task, time }: { task: string; time: string }) => {
   return (
     <li className={style.item}>
       <h3>{task}</h3>
@@ -8,5 +7,3 @@ const Item = ({ task, time }: { task: string; time: string }) => {
     </li>
   );
 };
-
-export default memo(Item);
---
+++ src/components/Stopwatch/index.tsx
@@ -0,0 +1,17 @@
+import Button from "../Button";
+import Clock from "./Clock";
+import style from "./Stopwatch.module.scss";
+
+const Stopwatch = () => {
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Clock />
+      </div>
+      <Button text="Começar!" />
+    </div>
+  );
+};
+
+export default Stopwatch;
